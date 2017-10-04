bd7545ca3b2e77815a1e7e6f4871899d44f8e421
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-10-04 21:19:47 +0200

    Playing with console log.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,3 @@
 /* Playing with JavaScript */
 
-var tacno = confirm("Da li je ovo tacno?");
-
-if (tacno === true) {
-    console.log("Strava radi");
-} else {
-    console.log("Ne radi kako treba");
-}
+console.table(["first", "second"]);
