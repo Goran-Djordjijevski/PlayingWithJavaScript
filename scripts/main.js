c71ace05de5ec32462b8ad43d3fe212f058ad108
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-10-07 14:39:18 +0200

    FizzBuzz example.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 /* Playing with JavaScript */
 
-var randomNumber = Math.round(Math.random() * 5);
+var randomNumber = Math.round(Math.random() * 15);
 
-if (randomNumber % 3 === 0) {
-    console.log("Fizz");
-} else if (randomNumber % 5 === 0) {
+if (randomNumber % 3 === 0 && randomNumber !== 0) {
+    if (randomNumber % 5 === 0) {
+        console.log("FizzBuzz");
+    } else {
+        console.log("Fizz");
+    }
+} else if (randomNumber % 5 === 0 && randomNumber !== 0) {
     console.log("Buzz");
 } else {
     console.log(randomNumber);
